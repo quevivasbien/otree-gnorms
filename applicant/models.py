adc4ae4fe8e2af8470628a16b335d1e8ebcf1151
quevivasbien <jensenm@uchicago.edu>
2021-11-02 14:26:24 -0500

    Added more ASVAB questions

--- applicant/models.py
+++ applicant/models.py
@@ -13,74 +13,81 @@ import random
 
 
 # load question text
-with open('_static/global/question_text.json', 'r',  encoding='utf-8') as fh:
+with open("_static/global/question_text.json", "r", encoding="utf-8") as fh:
     qtext = json.load(fh)
 # load constants
-with open('_static/global/constants.json', 'r',  encoding='utf-8') as fh:
+with open("_static/global/constants.json", "r", encoding="utf-8") as fh:
     constants = json.load(fh)
 
 
-author = 'Mckay D Jensen'
+author = "Mckay D Jensen"
 
 doc = """
 Gender norms of self-promotion
 """
 
-perform_pdf = constants['perform_cdf'][:1] \
-    + [constants['perform_cdf'][i] - constants['perform_cdf'][i-1] for i in range(1, len(constants['perform_cdf']))]
+perform_pdf = constants["perform_cdf"][:1] + [
+    constants["perform_cdf"][i] - constants["perform_cdf"][i - 1]
+    for i in range(1, len(constants["perform_cdf"]))
+]
 
 
 class Constants(BaseConstants):
-    name_in_url = 'applicant'
+    name_in_url = "applicant"
     players_per_group = None
     num_rounds = 1
-    estimated_time = constants['applicant_estimated_time']
-    payment = '{:.2f}'.format(constants['applicant_payment'])
-    max_bonus = '{:.2f}'.format(constants['applicant_max_bonus'])
-    mean_performance = '{:.1f}'.format(sum([x * i for i, x in enumerate(perform_pdf)]))
-    num_wg = constants['num_wg1'] + constants['num_wg2'] + constants['num_wg3']
+    estimated_time = constants["applicant_estimated_time"]
+    payment = "{:.2f}".format(constants["applicant_payment"])
+    max_bonus = "{:.2f}".format(constants["applicant_max_bonus"])
+    mean_performance = "{:.1f}".format(sum([x * i for i, x in enumerate(perform_pdf)]))
+    num_wg = constants["num_wg1"] + constants["num_wg2"] + constants["num_wg3"]
 
 
 class Subsession(BaseSubsession):
-
     def creating_session(self):
         for i, p in enumerate(self.get_players()):
             # assign treatment
             p.treatment = i % 3
             p.show_perf_guess = i % 2
             # assign question order
-            question_order = random.sample(range(1, 26), 20)
+            question_order = random.sample(range(1, 32), 20)
             random.shuffle(question_order)
-            p.question_order = '-'.join([str(x) for x in question_order])
+            p.question_order = "-".join([str(x) for x in question_order])
             # assign evaluation and non-application questions
             eval_qs = random.sample(question_order, 10)
             noneval_qs = [j for j in question_order if j not in eval_qs]
-            p.participant.vars['eval_qs'] = eval_qs
-            p.participant.vars['noneval_qs'] = noneval_qs
+            p.participant.vars["eval_qs"] = eval_qs
+            p.participant.vars["noneval_qs"] = noneval_qs
             # assign wage guess orderings
             num_wg = Constants.num_wg
             wg_gender = random.choices(["Male", "Female"], k=num_wg)
-            p.wage_guess_gender = '-'.join(map(str, wg_gender))
+            p.wage_guess_gender = "-".join(map(str, wg_gender))
             wg_image = random.choices(list(range(4)), k=num_wg)
-            p.wage_guess_image = '-'.join(map(str, wg_image))
+            p.wage_guess_image = "-".join(map(str, wg_image))
             wg_perform = random.choices(list(range(11)), k=num_wg)
-            p.wage_guess_perform = '-'.join(map(str, wg_perform))
-            wg_promote_type = [0] * constants['num_wg1'] + [1] * constants['num_wg2'] + [2] * constants['num_wg3']
+            p.wage_guess_perform = "-".join(map(str, wg_perform))
+            wg_promote_type = (
+                [0] * constants["num_wg1"]
+                + [1] * constants["num_wg2"]
+                + [2] * constants["num_wg3"]
+            )
             random.shuffle(wg_promote_type)
-            p.wage_guess_promote_type = '-'.join(map(str, wg_promote_type))
+            p.wage_guess_promote_type = "-".join(map(str, wg_promote_type))
             wg_promote1 = random.choices(list(range(6)), k=num_wg)
-            p.wage_guess_promote1 = '-'.join(map(str, wg_promote1))
+            p.wage_guess_promote1 = "-".join(map(str, wg_promote1))
             wg_promote2 = random.choices(list(range(101)), k=num_wg)
-            p.wage_guess_promote2 = '-'.join(map(str, wg_promote2))
+            p.wage_guess_promote2 = "-".join(map(str, wg_promote2))
             wg_promote3 = random.choices(list(range(3)), k=num_wg)
-            p.wage_guess_promote3 = '-'.join(map(str, wg_promote3))
+            p.wage_guess_promote3 = "-".join(map(str, wg_promote3))
 
 
 class Group(BaseGroup):
     pass
 
 
-understanding1_choices = [x.replace('~', Constants.payment) for x in qtext['understanding1']]
+understanding1_choices = [
+    x.replace("~", Constants.payment) for x in qtext["understanding1"]
+]
 
 
 class Player(BasePlayer):
@@ -99,55 +106,72 @@ class Player(BasePlayer):
     captcha = models.CharField(blank=True)
     # demographic survey questions
     age = models.IntegerField(min=18, max=95)
-    gender = models.StringField(choices=qtext['gender'])
+    gender = models.StringField(choices=qtext["gender"])
     # ethnicity = models.StringField(choices=qtext['ethnicity'])
     # home_state = models.StringField(choices=qtext['home_state'])
-    education = models.StringField(choices=qtext['education'])
+    education = models.StringField(choices=qtext["education"])
     # married = models.StringField(choices=qtext['married'])
     # household_income = models.StringField(choices=qtext['household_income'])
-    employed = models.StringField(choices=qtext['employed'])
+    employed = models.StringField(choices=qtext["employed"])
     # religion = models.StringField(choices=qtext['religion'])
     # politics = models.StringField(choices=qtext['politics'])
-    resident = models.StringField(choices=['Yes', 'No'])
+    resident = models.StringField(choices=["Yes", "No"])
     # understanding questions
-    understanding1 = models.StringField(choices=understanding1_choices, widget=widgets.RadioSelect)
-    understanding2 = models.StringField(choices=qtext['understanding2'], widget=widgets.RadioSelect)
-    understanding3 = models.StringField(choices=qtext['understanding3'], widget=widgets.RadioSelect)
-    understanding4 = models.StringField(choices=qtext['understanding4'], widget=widgets.RadioSelect)
+    understanding1 = models.StringField(
+        choices=understanding1_choices, widget=widgets.RadioSelect
+    )
+    understanding2 = models.StringField(
+        choices=qtext["understanding2"], widget=widgets.RadioSelect
+    )
+    understanding3 = models.StringField(
+        choices=qtext["understanding3"], widget=widgets.RadioSelect
+    )
+    understanding4 = models.StringField(
+        choices=qtext["understanding4"], widget=widgets.RadioSelect
+    )
     # ASVAB questions
-    q1 = models.StringField(choices=qtext['q1'], widget=widgets.RadioSelect)
-    q2 = models.StringField(choices=qtext['q2'], widget=widgets.RadioSelect)
-    q3 = models.StringField(choices=qtext['q3'], widget=widgets.RadioSelect)
-    q4 = models.StringField(choices=qtext['q4'], widget=widgets.RadioSelect)
-    q5 = models.StringField(choices=qtext['q5'], widget=widgets.RadioSelect)
-    q6 = models.StringField(choices=qtext['q6'], widget=widgets.RadioSelect)
-    q7 = models.StringField(choices=qtext['q7'], widget=widgets.RadioSelect)
-    q8 = models.StringField(choices=qtext['q8'], widget=widgets.RadioSelect)
-    q9 = models.StringField(choices=qtext['q9'], widget=widgets.RadioSelect)
-    q10 = models.StringField(choices=qtext['q10'], widget=widgets.RadioSelect)
-    q11 = models.StringField(choices=qtext['q11'], widget=widgets.RadioSelect)
-    q12 = models.StringField(choices=qtext['q12'], widget=widgets.RadioSelect)
-    q13 = models.StringField(choices=qtext['q13'], widget=widgets.RadioSelect)
-    q14 = models.StringField(choices=qtext['q14'], widget=widgets.RadioSelect)
-    q15 = models.StringField(choices=qtext['q15'], widget=widgets.RadioSelect)
-    q16 = models.StringField(choices=qtext['q16'], widget=widgets.RadioSelect)
-    q17 = models.StringField(choices=qtext['q17'], widget=widgets.RadioSelect)
-    q18 = models.StringField(choices=qtext['q18'], widget=widgets.RadioSelect)
-    q19 = models.StringField(choices=qtext['q19'], widget=widgets.RadioSelect)
-    q20 = models.StringField(choices=qtext['q20'], widget=widgets.RadioSelect)
-    q21 = models.StringField(choices=qtext['q21'], widget=widgets.RadioSelect)
-    q22 = models.StringField(choices=qtext['q22'], widget=widgets.RadioSelect)
-    q23 = models.StringField(choices=qtext['q23'], widget=widgets.RadioSelect)
-    q24 = models.StringField(choices=qtext['q24'], widget=widgets.RadioSelect)
-    q25 = models.StringField(choices=qtext['q25'], widget=widgets.RadioSelect)
+    q1 = models.StringField(choices=qtext["q1"], widget=widgets.RadioSelect)
+    q2 = models.StringField(choices=qtext["q2"], widget=widgets.RadioSelect)
+    q3 = models.StringField(choices=qtext["q3"], widget=widgets.RadioSelect)
+    q4 = models.StringField(choices=qtext["q4"], widget=widgets.RadioSelect)
+    q5 = models.StringField(choices=qtext["q5"], widget=widgets.RadioSelect)
+    q6 = models.StringField(choices=qtext["q6"], widget=widgets.RadioSelect)
+    q7 = models.StringField(choices=qtext["q7"], widget=widgets.RadioSelect)
+    q8 = models.StringField(choices=qtext["q8"], widget=widgets.RadioSelect)
+    q9 = models.StringField(choices=qtext["q9"], widget=widgets.RadioSelect)
+    q10 = models.StringField(choices=qtext["q10"], widget=widgets.RadioSelect)
+    q11 = models.StringField(choices=qtext["q11"], widget=widgets.RadioSelect)
+    q12 = models.StringField(choices=qtext["q12"], widget=widgets.RadioSelect)
+    q13 = models.StringField(choices=qtext["q13"], widget=widgets.RadioSelect)
+    q14 = models.StringField(choices=qtext["q14"], widget=widgets.RadioSelect)
+    q15 = models.StringField(choices=qtext["q15"], widget=widgets.RadioSelect)
+    q16 = models.StringField(choices=qtext["q16"], widget=widgets.RadioSelect)
+    q17 = models.StringField(choices=qtext["q17"], widget=widgets.RadioSelect)
+    q18 = models.StringField(choices=qtext["q18"], widget=widgets.RadioSelect)
+    q19 = models.StringField(choices=qtext["q19"], widget=widgets.RadioSelect)
+    q20 = models.StringField(choices=qtext["q20"], widget=widgets.RadioSelect)
+    q21 = models.StringField(choices=qtext["q21"], widget=widgets.RadioSelect)
+    q22 = models.StringField(choices=qtext["q22"], widget=widgets.RadioSelect)
+    q23 = models.StringField(choices=qtext["q23"], widget=widgets.RadioSelect)
+    q24 = models.StringField(choices=qtext["q24"], widget=widgets.RadioSelect)
+    q25 = models.StringField(choices=qtext["q25"], widget=widgets.RadioSelect)
+    q26 = models.StringField(choices=qtext["q26"], widget=widgets.RadioSelect)
+    q27 = models.StringField(choices=qtext["q27"], widget=widgets.RadioSelect)
+    q28 = models.StringField(choices=qtext["q28"], widget=widgets.RadioSelect)
+    q29 = models.StringField(choices=qtext["q29"], widget=widgets.RadioSelect)
+    q30 = models.StringField(choices=qtext["q30"], widget=widgets.RadioSelect)
+    q31 = models.StringField(choices=qtext["q31"], widget=widgets.RadioSelect)
+    q32 = models.StringField(choices=qtext["q32"], widget=widgets.RadioSelect)
     eval_correct = models.IntegerField()
     noneval_correct = models.IntegerField()
     # application questions
     avatar = models.StringField()
-    self_eval = models.StringField(choices=qtext['self_eval'], widget=widgets.RadioSelect)
+    self_eval = models.StringField(
+        choices=qtext["self_eval"], widget=widgets.RadioSelect
+    )
     self_eval_agree = models.IntegerField()
     self_eval_statement = models.StringField(
-        choices=qtext['self_eval_statement'], widget=widgets.RadioSelect
+        choices=qtext["self_eval_statement"], widget=widgets.RadioSelect
     )
     # guessing questions
     wage_guess1 = models.FloatField()
@@ -158,23 +182,25 @@ class Player(BasePlayer):
     perform_guess2 = models.IntegerField()
     perform_guess3 = models.IntegerField()
     perform_guess_other = models.StringField()
-    approp_guess1 = models.IntegerField()  # goes from 0 to 5, from v. soc. inappropriate to v. soc. appropriate
+    approp_guess1 = (
+        models.IntegerField()
+    )  # goes from 0 to 5, from v. soc. inappropriate to v. soc. appropriate
     approp_guess2 = models.IntegerField()
     approp_guess3 = models.IntegerField()
     approp_guess_other = models.StringField()
 
     def check_q(self, value, correct):
         if value != correct:
-            return 'Sorry. Your answer is incorrect. Please choose the correct answer to proceed.'
+            return "Sorry. Your answer is incorrect. Please choose the correct answer to proceed."
 
     def understanding1_error_message(self, value):
         return self.check_q(value, understanding1_choices[1])
 
     def understanding2_error_message(self, value):
-        return self.check_q(value, qtext['understanding2'][1])
+        return self.check_q(value, qtext["understanding2"][1])
 
     def understanding3_error_message(self, value):
-        return self.check_q(value, qtext['understanding3'][0])
+        return self.check_q(value, qtext["understanding3"][0])
 
     def understanding4_error_message(self, value):
-        return self.check_q(value, qtext['understanding4'][0])
+        return self.check_q(value, qtext["understanding4"][0])
--- applicant/pages.py
+++ applicant/pages.py
@@ -1,25 +1,26 @@
 # from otree.api import Currency as c, currency_range
 from ._builtin import Page
+
 # from .models import Constants
 from captcha.fields import ReCaptchaField
 
 import json
 
 # load question text
-with open('_static/global/question_text.json', 'r', encoding='utf-8') as fh:
+with open("_static/global/question_text.json", "r", encoding="utf-8") as fh:
     qtext = json.load(fh)
 
-with open('_static/global/constants.json', 'r', encoding='utf-8') as fh:
+with open("_static/global/constants.json", "r", encoding="utf-8") as fh:
     constants = json.load(fh)
 
 
 class Captcha(Page):
-    form_model = 'player'
-    form_fields = ['captcha']
+    form_model = "player"
+    form_fields = ["captcha"]
 
     def get_form(self, data=None, files=None, **kwargs):
         frm = super().get_form(data, files, **kwargs)
-        frm.fields['captcha'] = ReCaptchaField(label='')
+        frm.fields["captcha"] = ReCaptchaField(label="")
         return frm
 
 
@@ -28,28 +29,26 @@ class ConsentForm(Page):
 
 
 class Overview(Page):
-    form_model = 'player'
-    form_fields = ['understanding1']
+    form_model = "player"
+    form_fields = ["understanding1"]
 
 
 class DemographicSurvey(Page):
-    form_model = 'player'
-    form_fields = [
-        'age', 'gender', 'employed', 'education', 'resident'
-    ]
+    form_model = "player"
+    form_fields = ["age", "gender", "employed", "education", "resident"]
 
 
 class ASVABInstructions(Page):
-    form_model = 'player'
-    form_fields = ['understanding2']
+    form_model = "player"
+    form_fields = ["understanding2"]
 
 
 class ASVABQuestions(Page):
-    form_model = 'player'
-    form_fields = [f'q{i+1}' for i in range(25)]
+    form_model = "player"
+    form_fields = [f"q{i+1}" for i in range(25)]
 
     def vars_for_template(self):
-        return {'title': 'Part 1 of 4 - ASVAB Questions'}
+        return {"title": "Part 1 of 4 - ASVAB Questions"}
 
     def before_next_page(self):
         # checks player's ASVAB answers and figures out how many are correct
@@ -59,73 +58,114 @@ class ASVABQuestions(Page):
             3, 1, 2, 0, 3,
             3, 2, 0, 3, 2,
             2, 3, 1, 0, 0,
-            0, 2, 3, 1, 1
+            0, 2, 3, 1, 1,
+            0, 0, 2, 2, 3,
+            2, 3
         ]
         eval_correct = 0
         noneval_correct = 0
-        evqs = player.participant.vars['eval_qs']
-        nevqs = player.participant.vars['noneval_qs']
-        for i, q in enumerate([
-            player.q1, player.q2, player.q3, player.q4, player.q5,
-            player.q6, player.q7, player.q8, player.q9, player.q10,
-            player.q11, player.q12, player.q13, player.q14, player.q15,
-            player.q16, player.q17, player.q18, player.q19, player.q20,
-            player.q21, player.q22, player.q23, player.q24, player.q25
-        ]):
-            if i + 1 in evqs and q == qtext[f'q{i+1}'][correct_answers[i]]:
+        evqs = player.participant.vars["eval_qs"]
+        nevqs = player.participant.vars["noneval_qs"]
+        for i, q in enumerate(
+            [
+                player.q1,
+                player.q2,
+                player.q3,
+                player.q4,
+                player.q5,
+                player.q6,
+                player.q7,
+                player.q8,
+                player.q9,
+                player.q10,
+                player.q11,
+                player.q12,
+                player.q13,
+                player.q14,
+                player.q15,
+                player.q16,
+                player.q17,
+                player.q18,
+                player.q19,
+                player.q20,
+                player.q21,
+                player.q22,
+                player.q23,
+                player.q24,
+                player.q25,
+                player.q26,
+                player.q27,
+                player.q28,
+                player.q29,
+                player.q30,
+                player.q31,
+                player.q32
+            ]
+        ):
+            if i + 1 in evqs and q == qtext[f"q{i+1}"][correct_answers[i]]:
                 eval_correct += 1
-            elif i + 1 in nevqs and q == qtext[f'q{i+1}'][correct_answers[i]]:
+            elif i + 1 in nevqs and q == qtext[f"q{i+1}"][correct_answers[i]]:
                 noneval_correct += 1
         player.eval_correct = eval_correct
         player.noneval_correct = noneval_correct
 
 
 class ApplicationInstructions(Page):
-    form_model = 'player'
-    form_fields = ['understanding3']
+    form_model = "player"
+    form_fields = ["understanding3"]
 
     def vars_for_template(self):
         player = self.player
-        stage1_bonus = f'{player.eval_correct * 10} cents' if player.eval_correct < 10 else '$1.00'
+        stage1_bonus = (
+            f"{player.eval_correct * 10} cents" if player.eval_correct < 10 else "$1.00"
+        )
         return dict(stage1_bonus=stage1_bonus)
 
 
 class Application(Page):
-    form_model = 'player'
-    form_fields = ['avatar', 'self_eval',
-                   'self_eval_agree',
-                   'self_eval_statement']
+    form_model = "player"
+    form_fields = ["avatar", "self_eval", "self_eval_agree", "self_eval_statement"]
 
     def vars_for_template(self):
         # get the real performance pdf from the prelim questions we do
         # it's just a filler right now
-        perform_cdf = constants['perform_cdf']
-        better_than = int(perform_cdf[self.player.eval_correct]*100)
+        perform_cdf = constants["perform_cdf"]
+        better_than = int(perform_cdf[self.player.eval_correct] * 100)
         worse_than = 100 - better_than
         return dict(better_than=better_than, worse_than=worse_than)
 
 
 class GuessInstructions(Page):
-    form_model = 'player'
-    form_fields = ['understanding4']
+    form_model = "player"
+    form_fields = ["understanding4"]
 
 
 class WageGuess(Page):
-    form_model = 'player'
-    form_fields = ['wage_guess1', 'wage_guess2', 'wage_guess3', 'wage_guess_other']
+    form_model = "player"
+    form_fields = ["wage_guess1", "wage_guess2", "wage_guess3", "wage_guess_other"]
 
 
 class PerformanceGuess(Page):
-    form_model = 'player'
-    form_fields = ['perform_guess1', 'perform_guess2', 'perform_guess3', 'perform_guess_other']
+    form_model = "player"
+    form_fields = [
+        "perform_guess1",
+        "perform_guess2",
+        "perform_guess3",
+        "perform_guess_other",
+    ]
 
     def is_displayed(self):
         return self.player.show_perf_guess == 1
 
 
 class SocAppropGuess(Page):
-    form_model = 'player'
-    form_fields = ['approp_guess1', 'approp_guess2', 'approp_guess3', 'approp_guess_other']
+    form_model = "player"
+    form_fields = [
+        "approp_guess1",
+        "approp_guess2",
+        "approp_guess3",
+        "approp_guess_other",
+    ]
 
     def is_displayed(self):
         return self.player.show_perf_guess == 0
@@ -135,6 +175,18 @@ class CompletionCode(Page):
     pass
 
 
-page_sequence = [Captcha, ConsentForm, DemographicSurvey, Overview, ASVABInstructions,
-                 ASVABQuestions, ApplicationInstructions, Application,
-                 GuessInstructions, WageGuess, PerformanceGuess, SocAppropGuess, CompletionCode]
+page_sequence = [
+    Captcha,
+    ConsentForm,
+    DemographicSurvey,
+    Overview,
+    ASVABInstructions,
+    ASVABQuestions,
+    ApplicationInstructions,
+    Application,
+    GuessInstructions,
+    WageGuess,
+    PerformanceGuess,
+    SocAppropGuess,
+    CompletionCode,
+]
